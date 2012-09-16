50d80467e0434d1f70b7d37889d46532748ef35a
John Myles White <jmw@johnmyleswhite.com>
2012-09-16 15:04:10 -0400

    Missing reshape dependency

--- 12-Model_Comparison/chapter12.R
+++ 12-Model_Comparison/chapter12.R
@@ -43,6 +43,7 @@ mean(with(df, svm.predictions == Label))
 #[1] 0.7204
 
 # Third code snippet
+library("reshape")
 df <- cbind(df,
             data.frame(Logit = ifelse(predict(logit.fit) > 0, 1, 0),
                        SVM = ifelse(predict(svm.fit) > 0, 1, 0)))
