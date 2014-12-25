58ae3bf0fa404c02e7209f6acde52f1ff31d187a
Mark Needham <mark.needham@neotechnology.com>
2014-12-25 05:36:24 +0000

    TDM throws a 'no applicable method for meta' error due to incorrect to_lower conversion
    * Fixed by wrapping the call in 'content_transformer'
    * See http://stackoverflow.com/questions/25551514/termdocumentmatrix-errors-in-r

--- 06-Regularization/chapter06.R
+++ 06-Regularization/chapter06.R
@@ -393,7 +393,7 @@ documents <- data.frame(Text = ranks$Long.Desc.)
 row.names(documents) <- 1:nrow(documents)
 
 corpus <- Corpus(DataframeSource(documents))
-corpus <- tm_map(corpus, tolower)
+corpus <- tm_map(corpus, content_transformer(tolower))
 corpus <- tm_map(corpus, stripWhitespace)
 corpus <- tm_map(corpus, removeWords, stopwords('english'))
 
